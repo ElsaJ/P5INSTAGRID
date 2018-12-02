1247c5e8e757366b0967731fd24ac3645d82011b
ElsaJ <elsa.jeannin@gmail.com>
2018-12-02 15:09:54 +0100

    modified ImagesOrganizer

--- Instagrid/Instagrid/View/ImagesOrganizer.swift
+++ Instagrid/Instagrid/View/ImagesOrganizer.swift
@@ -23,20 +23,35 @@ class ImagesOrganizer: UIView {
     private func setStyle(_style: Style) {
         switch style {
         case .standard:
+            imageOne.frame = CGRect(x: 15, y: 16, width: imageOne.frame.size.width, height: imageOne.frame.size.height)
+            imageTwo.frame = CGRect(x: 166, y: 16, width: imageTwo.frame.size.width, height: imageTwo.frame.size.height)
+            imageThree.frame = CGRect(x: 15, y: 169, width: imageThree.frame.size.width, height: imageThree.frame.size.height)
             shapeOne.isHidden = false
             shapeTwo.isHidden = false
             shapeThree.isHidden = false
         case .reverse:
+            imageOne.frame = CGRect(x: 15, y: 169, width: imageOne.frame.size.width, height: imageOne.frame.size.height)
+            imageTwo.frame = CGRect(x: 166, y: 169, width: imageTwo.frame.size.width, height: imageTwo.frame.size.height)
+            imageThree.frame = CGRect(x: 15, y: 16, width: imageThree.frame.size.width, height: imageThree.frame.size.height)
             shapeOne.isHidden = false
             shapeTwo.isHidden = false
             shapeThree.isHidden = false
         case .square:
+            imageOne.frame = CGRect(x: 15, y: 16, width: imageOne.frame.size.width, height: imageOne.frame.size.height)
+            imageTwo.frame = CGRect(x: 166, y: 16, width: imageTwo.frame.size.width, height: imageTwo.frame.size.height)
+            imageThree.frame = CGRect(x: 15, y: 169, width: imageOne.frame.size.width, height: imageOne.frame.size.height)
             shapeOne.isHidden = false
             shapeTwo.isHidden = false
             shapeThree.isHidden = false
             
         }
     }
+    
+    internal func addSubview(_ view: ImagesOrganizer) {
+        
+    }
+        
+
     
     @IBOutlet private var imageOne: UIView!
     @IBOutlet private var imageTwo: UIView!
