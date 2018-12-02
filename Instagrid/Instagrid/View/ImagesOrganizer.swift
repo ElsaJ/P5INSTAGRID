326af18461e1c65d0876ca68fb8d90e5bc75d743
ElsaJ <elsa.jeannin@gmail.com>
2018-12-02 13:18:46 +0100

    create ImagesOrganizer class

---
+++ Instagrid/Instagrid/View/ImagesOrganizer.swift
@@ -0,0 +1,19 @@
+//
+//  ImagesOrganizer.swift
+//  Instagrid
+//
+//  Created by Elsa Jeannin on 02/12/2018.
+//  Copyright © 2018 Elsa Jeannin. All rights reserved.
+//
+
+import UIKit
+
+class ImagesOrganizer: UIView {
+
+    @IBOutlet private var imageOne: UIView!
+    @IBOutlet private var imageTwo: UIView!
+    @IBOutlet private var imageThree: UIView!
+    @IBOutlet private var shapeOne: UIButton!
+    @IBOutlet private var shapeTwo: UIButton!
+    @IBOutlet private var shapeThree: UIButton!
+}
